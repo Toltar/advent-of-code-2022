685e4f67f1264bba1bc86427413c2b2ceb68f2de
Toltar <matthew@matthew-dickens.com>
2022-12-02 15:58:57 -0800

    Implemented part 2

--- src/day02/src/main.rs
+++ src/day02/src/main.rs
@@ -22,6 +22,24 @@ impl Plays {
             Plays::Scissors => 3,
         }
     }
+
+    fn to_str(&self) -> &str {
+        match *self {
+            Plays::Paper => "Paper",
+            Plays::Rock => "Rock",
+            Plays::Scissors => "Scissors",
+        }
+    }
+}
+
+impl Clone for Plays {
+    fn clone(&self) -> Plays {
+        match self {
+            Plays::Rock => Plays::Rock,
+            Plays::Paper => Plays::Paper,
+            Plays::Scissors => Plays::Scissors,
+        }
+    }
 }
 
 enum OpponentsPlay {
@@ -144,6 +162,96 @@ fn part_one() {
     println!("Total score is {}", total_score);
 }
 
+enum RoundOutcome {
+    Win,
+    Loss,
+    Draw,
+}
+
+impl RoundOutcome {
+    fn value(&self) -> i32 {
+        match *self {
+            RoundOutcome::Loss => 0,
+            RoundOutcome::Win => 6,
+            RoundOutcome::Draw => 3,
+        }
+    }
+
+    fn from_str(input: &str) -> Option<RoundOutcome> {
+        match input {
+            "X" => Some(RoundOutcome::Loss),
+            "Y" => Some(RoundOutcome::Draw),
+            "Z" => Some(RoundOutcome::Win),
+            _ => None,
+        }
+    }
+
+    fn to_str(&self) -> &str {
+        match *self {
+            RoundOutcome::Draw => "Draw",
+            RoundOutcome::Loss => "Loss",
+            RoundOutcome::Win => "Win",
+        }
+    }
+}
+
+impl Clone for RoundOutcome {
+    fn clone(&self) -> RoundOutcome {
+        match self {
+            RoundOutcome::Draw => RoundOutcome::Draw,
+            RoundOutcome::Loss => RoundOutcome::Loss,
+            RoundOutcome::Win => RoundOutcome::Win,
+        }
+    }
+}
+
+fn determine_players_play(round_outcome: RoundOutcome, opponents_play: Plays) -> Plays {
+    let play = match round_outcome {
+        RoundOutcome::Draw => opponents_play.clone(),
+        RoundOutcome::Loss => match opponents_play {
+            Plays::Paper => Plays::Rock,
+            Plays::Rock => Plays::Scissors,
+            Plays::Scissors => Plays::Paper,
+        },
+        RoundOutcome::Win => match opponents_play {
+            Plays::Paper => Plays::Scissors,
+            Plays::Rock => Plays::Paper,
+            Plays::Scissors => Plays::Rock,
+        },
+    };
+    return play;
+}
+
+fn part_two() {
+    let file_contents = read_to_string("./input.txt").unwrap();
+    let lines = file_contents.lines();
+    let mut total_score = 0;
+    for line in lines {
+        let split: Vec<&str> = line.split(" ").collect();
+        let round_outcome = RoundOutcome::from_str(split[1]).unwrap();
+        let round_outcome_as_str = round_outcome.to_str();
+
+        let opponents_play = OpponentsPlay::from_str(split[0]).unwrap().value();
+        let opponents_play_as_str = opponents_play.to_str();
+
+        let players_play = determine_players_play(round_outcome.clone(), opponents_play.clone());
+
+        let round_score = players_play.value() + round_outcome.value();
+        total_score += round_score;
+        println!(
+            "Round Score: {}, Total Score: {}, Opponents Play {}, Players Play {}, Round Outcome: {}",
+            round_score,
+            total_score,
+            opponents_play_as_str,
+            players_play.to_str(),
+            round_outcome_as_str
+        );
+    }
+
+    println!("Total score for part 2 is {}", total_score);
+}
+
 fn main() {
     part_one();
+    part_two();
 }
